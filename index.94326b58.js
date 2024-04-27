cc67ec9a6cf5d98eb0e13587b64f05e4916759ad
Orest Kozak <orestkozak17@gmail.com>
2024-04-27 13:33:48 +0300

    make build

---
+++ index.94326b58.js
@@ -0,0 +1,2 @@
+var t=Array.from(document.querySelectorAll(".population")).map(function(t){return parseInt(t.textContent.replace(/,/g,""))}),e=t.reduce(function(t,e){return t+e},0),n=e/t.length;function r(t){return t.toString().replace(/\B(?=(\d{3})+(?!\d))/g,",")}var o=document.querySelector(".total-population"),u=document.querySelector(".average-population");o.textContent=r(e),u.textContent=r(n);
+//# sourceMappingURL=index.94326b58.js.map
